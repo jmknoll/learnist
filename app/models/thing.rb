f4be497899159194dab2ec19eed477b2aa9665c1
Jameson Knoll <jmknoll@Xiaoyi-Huangs-iMac.local>
2014-11-12 00:29:20 +0800

    model associations and validations

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,6 @@
 class Thing < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, presence: true
+
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, uniqueness: true
+
+	has_many :things
 end
