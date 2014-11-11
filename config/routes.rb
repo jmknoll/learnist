aa9a081ec70735acd15c6985e49bdd2edcff8638
Jameson Knoll <jmknoll@Xiaoyi-Huangs-iMac.local>
2014-11-11 23:46:54 +0800

    basic routing and static views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  get '/about' to: 'static_pages#about'
+
+  resources: users
+  resources: things
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
